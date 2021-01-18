de59363a4b56a2fa40931057bfe8a6f386c19896
frtru <francoistrudel13@gmail.com>
2021-01-18 00:11:07 -0500

    Added a simple handmade ObjectPool class to be used with Tasks. Need to add some more tests for that pool to make sure it doesnt break.

--- app/app.cpp
+++ app/app.cpp
@@ -1,4 +1,5 @@
 #include "engine/TaskEngine.h"
+#include "engine/Pools.h"
 
 #include <chrono>
 #include <iostream>
@@ -24,27 +25,68 @@ struct Yahoo
 
 int main(int argc, char* argv[])
 {
-  Yahoo y;
+  //Yahoo y;
 
-  Task* realTask = new Task(TaskPriority::MEDIUM);
-  realTask->Bind(&Yahoo::Bar, &y, 1.23f, "string");
+  //Task* realTask = new Task(TaskPriority::MEDIUM);
+  //realTask->Bind(&Yahoo::Bar, &y, 1.23f, "string");
 
-  Task *realTask2 =  new Task(TaskPriority::HIGH);
-  realTask2->Bind(baz, "mofo");
+  //Task *realTask2 =  new Task(TaskPriority::HIGH);
+  //realTask2->Bind(baz, "mofo");
 
-  TaskEngine::GetInstance()->RegisterRoutine(realTask);
-  TaskEngine::GetInstance()->RegisterRoutine(realTask2);
+  //TaskEngine::GetInstance()->RegisterRoutine(realTask);
+  //TaskEngine::GetInstance()->RegisterRoutine(realTask2);
 
-  y.Bar(0.1f, "yoyo");
+  //y.Bar(0.1f, "yoyo");
 
-  while (true) {
-    using namespace std::chrono_literals;
-    TaskEngine::GetInstance()->Update();
-    std::this_thread::sleep_for(1000ms);
+  //while (true) {
+  //  using namespace std::chrono_literals;
+  //  TaskEngine::GetInstance()->Update();
+  //  std::this_thread::sleep_for(1000ms);
+  //}
+
+  //delete realTask;
+  //delete realTask2;
+
+  struct A
+  {
+    explicit A(std::string i) : _i(i) {}
+    std::string _i;
+  };
+
+  ObjectPool<A, false> pool(1000, "hello");
+  
+  std::vector<A*> v1, v2;
+  while (pool.FreeChunksCount() > 0) {
+    v1.push_back(pool.Create("world"));
   }
 
-  delete realTask;
-  delete realTask2;
+  for (A* item : v1) {
+    std::cout << item->_i << std::endl;
+  }
+
+  unsigned long long i = 0;
+  while (v1.size() > 0) {
+    ++i;
+    pool.Release(std::move(v1.back()));
+    v1.pop_back();
+  }
+  std::cout << i << std::endl;
+
+  while (pool.FreeChunksCount() > 0) {
+    v2.push_back(pool.Create("yooy"));
+  }
+
+  v2.push_back(pool.Create("woops"));
+
+  for (A* item : v2) {
+    std::cout << item->_i << std::endl;
+  }
+
+  while (v2.size() > 0) {
+    ++i;
+    pool.Release(std::move(v2.back()));
+    v2.pop_back();
+  }
 
   return 0;
 }
--- engine/Pools.h
+++ engine/Pools.h
@@ -1,35 +1,51 @@
 #pragma once
 
-// NOTE: Flexible but costly pool and cannot allocate 
-// in blocks, in a contiguous and efficient manner. It
-// can easily be tweaked or be derived from to enhance
+// Those are just a few simple designs of object pools to 
+// avoid reallocating memory uselessly using STL containers.
+// For a more optimal implementation, you'd wanna go more
+// towards aligned memory pool allocators that is on par with
+// boost::pool_allocator. Maybe even implement your own alloc
+// policy for your buffers to prevent fragmentation in your 
+// own buffer.
+//
+// - DynamicPointerPool -
+// Allocate on heap for every new item and register
+// its pointer so this particular space can be reused.
+// It can easily be tweaked or be derived from to enhance
 // the properties
+//
+// +++  Flexibility
+// ++   Error handling when creating objects
+// +    Reuse of already allocated space
+// ---  No cache locality (non-contiguous allocations)
+// ---  Doesnt prevent fragmentation
+// --   Cannot allocate blocks (although it could be implemented)
 template<typename Type>
-class ResizeablePointerPool
+class DynamicPointerPool
 {
 public:
-  ResizeablePointerPool() {}
+  DynamicPointerPool() {}
   //delete all of the available memory chunks:
-  ~ResizeablePointerPool() {
+  ~DynamicPointerPool() {
     while (!m_pool.empty()) {
-      ::operator delete(m_pool.top());
+      delete(m_pool.top());
       m_pool.pop();
     }
   }
 
-  ResizeablePointerPool(const ResizeablePointerPool &) = delete;
-  ResizeablePointerPool(ResizeablePointerPool && other)
+  DynamicPointerPool(const DynamicPointerPool &) = delete;
+  DynamicPointerPool(DynamicPointerPool && other)
     : m_pool(std::move(other.m_pool)) {}
 
-  ResizeablePointerPool& operator=(const ResizeablePointerPool&) = delete;
-  ResizeablePointerPool& operator=(ResizeablePointerPool&& other) {
+  DynamicPointerPool& operator=(const DynamicPointerPool&) = delete;
+  DynamicPointerPool& operator=(DynamicPointerPool&& other) {
     this->m_pool(std::move(other.m_pool));
   }
 
   // Create an instance of Type:
   template<typename... Args>
   Type* Create(Args && ...args) {
-    Type *place = (Type*)(Allocate());
+    Type *place = Allocate();
 
     try {
       new (place) Type(std::forward<Args>(args)...);
@@ -43,11 +59,17 @@ public:
   }
 
   // Remove an instance of Type (add memory to the pool):
-  void Remove(Type* obj) {
+  void Release(Type* obj) {
     obj->~Type();
-    m_pool.push(obj);
+    Deallocate(obj);
   }
 
+
+  std::size_t FreeChunksCount() {
+    return m_pool.size();
+  }
+
+private:
   // Allocate a chunk of memory as big as Type needs:
   Type* Allocate() {
     Type *place;
@@ -68,72 +90,75 @@ public:
     m_pool.push(obj);
   }
 
-  std::size_t FreeChunksCount() {
-    return m_pool.size();
-  }
-
-private:
-
   //stack to hold pointers to free chunks:
   std::stack<Type*> m_pool;
 };
 
-// NOTE: For simplicity, Type will require a default
-// constructor implementation. TODO: check with args forwarding
-// if we can do something
-template<typename Type>
-class ResizableContiguousPool
+// - ObjectPool -
+// Uses a vector of objects (not pointers) as its underlying
+// container. The vector guarantees contiguity of the 
+// memory space used and offers better cache locality for smaller
+// objects (still remains a Array of Structures implementation, so
+// not as efficient as a Structure of Arrays design for caching).
+// You can have a fixed or dynamic size at the cost of a IsEmpty
+// check at each creation and a risk of an expensive memcpy to 
+// allocate a bigger buffer (normal vector behavior for capacity
+// alloc of 2 times the previous capacity).
+//
+// +++  Flexibility
+// +++  Reuse of already allocated space
+// +++  Contiguous
+// +++  Prevents fragmentation
+// --   Somewhat expensive copy operations to readd to pool
+// --   Cannot allocate blocks (although it could be implemented)
+// -    Voluntarily has no error handling (could be added though)
+template<typename Type, bool DynamicSize = true>
+class ObjectPool
 {
 public:
-  ResizableContiguousPool(std::size_t itemCount)
-    : m_pool(std::vector<Type>(itemCount)) {}
-  //delete all of the available memory chunks:
-  ~ResizableContiguousPool() {
-    // Let the vector do its job
-  }
+  template <typename... Args>
+  explicit ObjectPool(std::size_t itemCount, Args&&... args)
+    : m_pool(std::vector<Type>(itemCount, Type(args...))) {}
+  
+  // Vector can take care of deleting everything
+  ~ObjectPool() = default;
 
-  ResizableContiguousPool(const ResizableContiguousPool &) = delete;
-  ResizableContiguousPool(ResizableContiguousPool && other)
+  ObjectPool(const ObjectPool &) = delete;
+  ObjectPool(ObjectPool && other)
     : m_pool(std::move(other.m_pool)) {}
 
-  ResizableContiguousPool& operator=(const ResizableContiguousPool&) = delete;
-  ResizableContiguousPool& operator=(ResizableContiguousPool&& other) {
+  ObjectPool& operator=(const ObjectPool&) = delete;
+  ObjectPool& operator=(ObjectPool&& other) {
     this->m_pool(std::move(other.m_pool));
   }
 
-  // Create an instance of Type:
   template<typename... Args>
   Type* Create(Args && ...args) {
-    Type *place = (Type*)(Allocate());
-
-    try {
-      new (place) Type(std::forward<Args>(args)...);
+    // This is the resizeable portion. This of course
+    // affects the performance, but allows for a more
+    // flexible use.
+    if (m_pool.empty()) {
+      if constexpr (DynamicSize)
+        return &m_pool.emplace(args...);
+      else
+        return nullptr;
     }
-    catch (...) {
-      m_pool.push(*place);
-      throw;
-    }
-
-    return place;
+    return new (Allocate()) Type(std::forward<Args>(args)...);
   }
 
-  // Remove an instance of Type (add memory to the pool):
-  void Remove(Type* obj) {
-    // TODO:
+  // Replace the pointed object and inserts a COPY
+  // of the object into the vector. Which means objects
+  // must be copy-able to use this method.
+  void Release(Type* obj) {
+    obj->~Type();
+    m_pool.push(*obj);
   }
 
-  // Allocate a chunk of memory as big as Type needs:
-  Type* Allocate() {
-    Type *place;
-
-    // TODO:
-
-    return place;
-  }
-
-  //Keeping as a utility
-  void Deallocate(Type* obj) {
-    // TODO:
+  // rvalue equivalent to avoid an expensive copy
+  // for objects that support it.
+  void Release(Type&& obj) {
+    obj.~Type();
+    m_pool.push(obj);
   }
 
   std::size_t FreeChunksCount() {
@@ -142,5 +167,30 @@ public:
 
 private:
 
+  Type* Allocate() {
+    Type *place = &m_pool.top();
+    m_pool.pop();
+
+    return place;
+  }
+
   std::stack<Type, std::vector<Type> > m_pool;
 };
+
+// TODO: Draft of PoolableObject if ever necessary
+//
+//template <std::size_t PoolSize, PoolType = ObjectPool>
+//class PoolableObject
+//{
+//public:
+//  static void* operator new(std::size_t count) {
+//
+//  }
+//
+//  static void* operator new(std::size_t count) {
+//
+//  }
+//
+//protected:
+//  static PoolType<PoolableObject> s_pool;
+//};
